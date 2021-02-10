462c25e2906dce72c042ae4272c384e0e46927d5
AdBoc <adrianboczon98@gmail.com>
2021-02-10 14:37:14 +0100

    added meta description for SEO

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,6 +7,7 @@ const Layout = ({children}) => {
         <title>SpaceX Graph</title>
         <link rel="icon" href="/favicon.ico"/>
         <meta name="author" content="Adrian Boczoń"/>
+        <meta name="description" content="spacex masses of payloads on chart"/>
       </Head>
       <main>
         {children}
